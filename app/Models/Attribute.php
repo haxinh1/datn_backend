2d4fc921ca0105de6da5dff7d2a9b7223fccea27
congmd0504 <congmd0504@gmail.com>
2025-01-08 06:44:03 +0700

    attributes

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attribute extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_variant',
+        'is_active',
+    ];
 }

09a1f9c43e4120bd4cf68e9aeb6bec6482927b98
VietAnh464 <viethoak1@gmail.com>
2025-04-08 08:53:47 +0700

    cập nhật logic trả hàng hàng và hoàn tiền theo %coupon

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -13,25 +13,19 @@ class CartItem extends Model
         'user_id', 'product_id', 'product_variant_id', 'quantity', 'price'
     ];
 
-    /**
-     * 📌 Quan hệ: Một mục giỏ hàng thuộc về một người dùng (nếu đã đăng nhập)
-     */
+    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    /**
-     * 📌 Quan hệ: Một mục giỏ hàng thuộc về một sản phẩm
-     */
+    
     public function product()
     {
         return $this->belongsTo(Product::class);
     }
 
-    /**
-     * 📌 Quan hệ: Một mục giỏ hàng thuộc về một biến thể sản phẩm (nếu có)
-     */
+    
     public function productVariant()
     {
         return $this->belongsTo(ProductVariant::class);

9f9a79a5ebd75709c201c9f5ea514380650c5803
haxinh1 <hanguyen010129@gmail.com>
2025-04-28 09:19:16 +0700

    update category

--- routes/api.php
+++ routes/api.php
@@ -256,7 +256,7 @@ Route::get('categories', [CategoryController::class, 'index']); // Lấy danh s�
 Route::get('product-by-category/{id}', [CategoryController::class, 'getProductByCategory']); // Lấy danh sách danh mục
 Route::get('categories/{category}', [CategoryController::class, 'show']); // Lấy thông tin danh mục cụ thể
 Route::post('categories/create', [CategoryController::class, 'create']);
-Route::put('categories/update/{id}', [CategoryController::class, 'update']);
+Route::put('categories/update/{category}', [CategoryController::class, 'update']);
 Route::put('categories/update-status/{category}', [CategoryController::class, 'updateStatus']);
 Route::delete('categories/delete/{category}', [CategoryController::class, 'destroy']);
 
